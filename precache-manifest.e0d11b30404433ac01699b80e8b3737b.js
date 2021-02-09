7b5ffc3247f53657547a4125592c3843ffee1e09
matincodes <abdulmatinadeniji@gmail.com>
2021-02-09 12:56:29 +0100

    Updates

---
+++ precache-manifest.e0d11b30404433ac01699b80e8b3737b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42efdb435be6c70b55bf1ed2abe6ed3c",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "90b4b4f34f190318dbeb",
+    "url": "/birthday/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b4b4f34f190318dbeb",
+    "url": "/birthday/static/js/main.e780be53.chunk.js"
+  },
+  {
+    "revision": "3db92290b359abe36f89",
+    "url": "/birthday/static/js/runtime-main.d025061d.js"
+  }
+]);
